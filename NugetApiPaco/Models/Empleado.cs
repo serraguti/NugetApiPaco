ec97c864f1caf1e15fbbab09f735fca13be98cbb
serraguti <pacoserranox@gmail.com>
2023-04-14 10:04:59 +0200

    Add project files.

---
+++ NugetApiPaco/Models/Empleado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetApiPaco.Models
+{
+    [Table("EMP")]
+    public class Empleado
+    {
+        [Key]
+        [Column("EMP_NO")]
+        public int IdEmpleado { get; set; }
+        [Column("APELLIDO")]
+        public string Apellido { get; set; }
+        [Column("OFICIO")]
+        public string Oficio { get; set; }
+        [Column("SALARIO")]
+        public int Salario { get; set; }
+        [Column("DEPT_NO")]
+        public int IdDepartamento { get; set; }
+    }
+}
